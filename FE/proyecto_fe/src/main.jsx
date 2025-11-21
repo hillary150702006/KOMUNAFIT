988116612cf85d02ccad7fed6704b234ef3eebff
hilla19.fh <hillaryfwd19@gmail.com>
2025-11-20 23:35:53 -0600

    el navbar sirve

--- FE/proyecto_fe/src/main.jsx
+++ FE/proyecto_fe/src/main.jsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
-import Routing from './routes/Routing'
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Routing/>
+    <App />
   </StrictMode>,
 )
